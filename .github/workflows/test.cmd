fb9b00fcf74dc1c9bf4a0e7ef521f34237e1ba89
Randy Fay <randy@randyfay.com>
2021-01-04 08:20:23 -0700

    Use a test script

---
+++ .github/workflows/test.cmd
@@ -0,0 +1,10 @@
+@echo "Building using bash and build.sh"
+@pwd
+"C:\Program Files\git\bin\bash" .github/workflows/test.sh
+
+if %ERRORLEVEL% EQU 0 (
+   @echo Successful build
+) else (
+   @echo Failure Reason Given is %errorlevel%
+   exit /b %errorlevel%
+)
---
+++ .github/workflows/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script is used to build drud/ddev using buildkite
+
+echo "--- github actions building at $(date) on $(hostname) for OS=$(go env GOOS) in $PWD with golang=$(go version) docker=$(docker version --format '{{.Server.Version}}') and docker-compose $(docker-compose version --short) ddev version=$(ddev --version)"
+
+export GOTEST_SHORT=1
+export DRUD_NONINTERACTIVE=true
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -x
+
+# On macOS, restart docker to avoid bugs where containers can't be deleted
+if [ "${OSTYPE%%[0-9]*}" = "darwin" ]; then
+  killall Docker || true
+  nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+  sleep 10
+fi
+
+# Make sure docker is working
+echo "Waiting for docker to come up: $(date)"
+date && timeout -v 10m bash -c 'while ! docker ps >/dev/null 2>&1 ; do
+  sleep 10
+  echo "Waiting for docker to come up: $(date)"
+done'
+echo "Testing again to make sure docker came up: $(date)"
+if ! docker ps >/dev/null 2>&1 ; then
+  echo "Docker is not running, exiting"
+  exit 1
+fi
+
